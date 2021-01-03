7ca80b55f9f923992454e4bb09a5f2a5dee7081c
Henry Lao <hlao1995@gmail.com>
2021-01-03 03:46:17 -0800

    init

---
+++ Prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Henry Lao on 1/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
